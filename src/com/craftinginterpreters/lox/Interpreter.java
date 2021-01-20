d4285c631004bd7424992a7c92f9f5d4bb326ae0
xx01cyx <caoyuanxin0531@outlook.com>
2021-01-20 17:22:27 +0800

    Enable addition for string and other types

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -11,17 +11,6 @@ class Interpreter implements Expr.Visitor<Object> {
         }
     }
 
-    String stringify(Object object) {
-        if (object == null)  return "nil";
-        if (object instanceof Double) {
-            String text = object.toString();
-            if (text.endsWith(".0"))
-                text = text.substring(0, text.length() - 2);
-            return text;
-        }
-        return object.toString();
-    }
-
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
@@ -60,7 +49,11 @@ class Interpreter implements Expr.Visitor<Object> {
                     return (double)left + (double)right;
                 if (left instanceof String && right instanceof String)
                     return (String)left + (String)right;
-                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
+                if (left instanceof String)
+                    return (String)left + stringify(right);
+                if (right instanceof String)
+                    return stringify(left) + (String)right;
+                throw new RuntimeError(expr.operator, "Operands must be addable.");
             case MINUS:
                 checkNumberOperands(expr.operator, left, right);
                 return (double)left - (double)right;
@@ -128,5 +121,16 @@ class Interpreter implements Expr.Visitor<Object> {
     private void checkNumberOperands(Token operator, Object left, Object right) {
         if (left instanceof Double && right instanceof Double)  return;
         throw new RuntimeError(operator, "Operands must be numbers.");
+    }
+
+    private String stringify(Object object) {
+        if (object == null)  return "nil";
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0"))
+                text = text.substring(0, text.length() - 2);
+            return text;
+        }
+        return object.toString();
     }
 }
